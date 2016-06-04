2531394082a47694a2e1984983584c33b424a03c
Petr Pulc <petrpulc@gmail.com>
2016-06-04 14:45:21 +0200

    Enable whitespace-insensitivity
    Enable to compare blobs insensitively to either leading or both leading and trailing whitespaces.

--- gitcmp.py
+++ gitcmp.py
@@ -12,6 +12,7 @@ parser.add_argument('--pedantic', '-p', action='store_true', help='checked repos
 parser.add_argument('--author', '-a', action='store_true', help='chech authorship details of commits as well (l>=commit)')
 parser.add_argument('--references', '-r', metavar='refs', nargs='+', help='check only selected references')
 parser.add_argument('--reject-msg', metavar='regex', help='reject commit when "regex" found in commit message')
+parser.add_argument('--ignore-whitespace', choices=['leading','both'], default='none', help='ignore leading whitespace in blobs')
 
 args = parser.parse_args()
 
@@ -106,14 +107,27 @@ print("\n=== Blobs")
 import difflib
 d = difflib.Differ()
 
-for o_blob, n_blob in blob_mapping.iteritems():
-    print("  Blob {}:".format(n_blob))
-    if o_blob != n_blob:
+def comp_n_diff(cmp1, cmp2, diff1, diff2, divider, note):
+    if cmp1 != cmp2:
         print("    Contents of file {} in commit {} do not match!".format(blob_info[o_blob]['path'], blob_info[o_blob]['commit']))
         if args.v:
-            print("Diff:")
-            print(''.join(d.compare(original[o_blob].data.splitlines(1), new[n_blob].data.splitlines(1))))
+            print("Diff{}:".format(note))
+            print(divider.join(d.compare(diff1, diff2)))
         exit(1)
+
+for o_blob, n_blob in blob_mapping.iteritems():
+    print("  Blob {}:".format(n_blob))
+
+    if args.ignore_whitespace == 'none':
+        comp_n_diff(o_blob, n_blob, original[o_blob].data.splitlines(1), new[n_blob].data.splitlines(1), '', '')
+    elif args.ignore_whitespace == 'leading':
+        data1 = [l.strip(' \t') for l in original[o_blob].data.splitlines(1)]
+        data2 = [l.strip(' \t') for l in new[n_blob].data.splitlines(1)]
+        comp_n_diff(data1, data2, data1, data2, '', ' (leading whitespace ignored)')
+    elif args.ignore_whitespace == 'both':
+        data1 = [l.strip() for l in original[o_blob].data.splitlines()]
+        data2 = [l.strip() for l in new[n_blob].data.splitlines()]
+        comp_n_diff(data1, data2, data1, data2, '', ' (whitespace ignored)')
 
 print("  OK")
 print("\nRepositories match.")
