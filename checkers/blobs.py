e65825fe5e0c95e9fc3c698a2c2bb9d3587f2cb4
Petr Pulc <pulcpetr@fit.cvut.cz>
2020-01-16 10:22:29 +0100

    HOTFIX: remove colorama
    TODO: investigate issues on MacOSX

--- checkers/blobs.py
+++ checkers/blobs.py
@@ -5,11 +5,7 @@ Blob level checker (individual files and file contents).
 import sys
 from difflib import unified_diff
 
-import colorama
-
 from common import Common
-
-colorama.init()
 
 
 def __comp_n_diff(data1, data2, blob_sha, note):
@@ -22,30 +18,10 @@ def __comp_n_diff(data1, data2, blob_sha, note):
           format(path, Common.blobs_info[blob_sha]['commit']))
     if Common.args.verbose:
         try:
-            diff = list(unified_diff(data1, data2, 'a' + path, 'b' + path))
+            diff = unified_diff(data1, data2, 'a' + path, 'b' + path)
 
             print("Diff{}:".format(note))
-            sys.stdout.write(colorama.Style.BRIGHT)
-            for row in diff[:2]:
-                sys.stdout.write(row)
-            sys.stdout.write(colorama.Style.RESET_ALL)
-
-            sys.stdout.write(colorama.Fore.CYAN)
-            sys.stdout.write(diff[2])
-            sys.stdout.write(colorama.Style.RESET_ALL)
-
-            for row in diff[3:]:
-                if row.startswith('+'):
-                    sys.stdout.write(colorama.Fore.GREEN)
-                if row.startswith('-'):
-                    sys.stdout.write(colorama.Fore.RED)
-
-                clean = row.rstrip()
-                sys.stdout.write(clean)
-                if len(row) > 2 and len(clean) < len(row) - 1:
-                    sys.stdout.write(colorama.Back.RED)
-                sys.stdout.write(row[len(clean):])
-                sys.stdout.write(colorama.Style.RESET_ALL)
+            print(''.join(diff))
 
         except TypeError:
             print("Binary files, no diff to be shown.")
